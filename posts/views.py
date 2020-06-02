2382af2110a2e0afad0803119d22e1f0d7ba75b9
THE CODING ZONE <sweetsanjeevkumar96@gmail.com>
2020-06-03 01:03:56 +0530

    implement api for CRUD operations

---
+++ posts/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+from .models import Posts
+from .serializers import PostSerializer
+
+# Create your views here.
+class PostList(generics.CreateAPIView):
+  queryset = Posts.objects.all()
+  serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Posts.objects.all()
+  serializer_class = PostSerializer
