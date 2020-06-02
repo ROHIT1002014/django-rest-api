eaa98c907a66433bbfca1932198b5a98acf66c20
THE CODING ZONE <sweetsanjeevkumar96@gmail.com>
2020-06-02 23:33:16 +0530

    use api in vuejs project

---
+++ django-api-vue/src/views/About.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="about">
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+          <h1>
+            {{ post.id }}
+          </h1>
+          <h1>
+            {{ post.title }}
+          </h1>
+          <p>
+            {{ post.body }}
+          </p>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+ created() {
+    axios.get(`http://127.0.0.1:8000/api/`)
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.posts = response.data
+      console.log(this.posts)
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  },
+}
+</script>
