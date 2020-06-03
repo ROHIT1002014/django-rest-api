76a53b66073b66743fe23ad4c2f0b3b499419c69
THE CODING ZONE <sweetsanjeevkumar96@gmail.com>
2020-06-03 21:25:31 +0530

    added authenticated permission in api

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,14 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Posts
 from .serializers import PostSerializer
 
 # Create your views here.
 class PostList(generics.CreateAPIView):
+  permission_classes = [permissions.IsAuthenticated]
   queryset = Posts.objects.all()
   serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes = [permissions.IsAuthenticated]
   queryset = Posts.objects.all()
   serializer_class = PostSerializer
