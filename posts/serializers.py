45ba75328aa68eaef7851efbbecd524a58a2bd6f
THE CODING ZONE <sweetsanjeevkumar96@gmail.com>
2020-06-05 23:44:35 +0530

    concept of repetation of code and to remove this we have to viewset

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import serializers
 from .models import Posts
 
@@ -5,3 +7,8 @@ class PostSerializer(serializers.ModelSerializer):
   class Meta:
     model = Posts
     fields = '__all__'
+
+class UserSErializer(serializers.ModelSerializer):
+  class Meta:
+    model = get_user_model()
+    fields = ('id', 'username')
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import generics, permissions
 from .models import Posts
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSErializer
 from .permissions import IsAuthorOrReadOnly
 
 # Create your views here.
@@ -13,3 +15,12 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
   permission_classes = [IsAuthorOrReadOnly,]
   queryset = Posts.objects.all()
   serializer_class = PostSerializer
+
+# if u see below code is the repetation of above code
+class UserList(generics.ListCreateAPIView):
+  queryset = get_user_model().objects.all()
+  serializer_class = UserSErializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = get_user_model().objects.all()
+  serializer_class = UserSErializer
